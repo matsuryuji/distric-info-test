965fd922f73aaebf67f421a961b96a977ff6d146
matsuryuji <mrmatsutane@hotmail.com>
2023-01-19 03:02:21 -0300

    fix: center position

--- src/components/PolygonMap/PolygonMap.jsx
+++ src/components/PolygonMap/PolygonMap.jsx
@@ -5,7 +5,7 @@ import "leaflet/dist/leaflet.css";
 const PolygonMap = (props) => {
   return (
     <div className="polygon-map__wrapper">
-      <MapContainer center={[-23.219578, -45.899868]} zoom={14}>
+      <MapContainer center={[-23.21723, -45.900956]} zoom={14}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
